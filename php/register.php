7c8db3def393dbf7b912d519857ba36754ca29ae
Marcus <marcustxl97@gmail.com>
2021-10-23 16:24:36 +0800

    Update 1.23
    Updated username

--- php/register.php
+++ php/register.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // register.php
 //include "dbconnect.php";
 if (!empty($_POST)) { // check if there is POST data. If not, do nothing.
@@ -17,12 +17,12 @@ if (!empty($_POST)) { // check if there is POST data. If not, do nothing.
   echo "<script>";
 
   if (isset($_POST['submit'])) {
-    if (empty($_POST['name']) || empty ($_POST['password'])
+    if (empty($_POST['username']) || empty ($_POST['password'])
       || empty ($_POST['email']) || empty ($_POST['tel'])) {
     echo "All records to be filled in";
     exit;}
     }
-  $name = $_POST['name'];
+  $username = $_POST['username'];
   $password = $_POST['password'];
   $email = $_POST['email'];
   $tel = $_POST['tel'];
@@ -31,7 +31,7 @@ if (!empty($_POST)) { // check if there is POST data. If not, do nothing.
   //TODO: put a catcher to catch empty form. follow menu.php from case study 4
   $password = md5($password);
   // echo $password;
-  $sql = "INSERT INTO shrek_users (name, password, email, tel) VALUES ('".$name."', '".$password."', '".$email."', '".$tel."')";
+  $sql = "INSERT INTO shrek_users (username, password, email, tel) VALUES ('".$username."', '".$password."', '".$email."', '".$tel."')";
   if (mysqli_query($conn, $sql)) {
     // insert operation successful
   } else {
