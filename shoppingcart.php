83986d9cb0feb9085d6d405f798e2060e197ee5e
Marcus <marcustxl97@gmail.com>
2021-10-26 23:29:59 +0800

    Update 1.42
    Fix shopping cart table
    - clear cart button done
    
    To do:
    - payment page & email function
    - split to men/women category

--- shoppingcart.php
+++ shoppingcart.php
@@ -15,6 +15,7 @@ echo '<script src="js/nav-login.js"></script>';
 
 $product_id=$_GET['product_id'];
 
+// Query 1 - Product information that user has added to cart
 $query = 'select * from products '
            ."where product_id='$product_id' ";
 
@@ -29,6 +30,7 @@ if ($result->num_rows > 0) {
   }
 }
 
+// Query 2 - Userid of user that is currently logged in
 $query2 = 'select customer_id from shrek_users '
            ."where username='$userid' ";
 
@@ -40,56 +42,66 @@ if ($result->num_rows > 0) {
   }
 }
 
+// Query 3 - Inserting selected product in user's shopping cart
 $query3 = "INSERT INTO shoppingcart(`customer_id`, `product_id`, `product_brand`, `product_name`, `price`) VALUES ($user_id, $id, '$brand', '$name', $amount)";
 $result = $dbcnx->query($query3);
 
+// Query 4 - Selecting all product from a certain user's shopping cart to display
 $query4 = 'select * from shoppingcart '
            ."where customer_id='$customerid' ";
 
 $result = $dbcnx->query($query4);
 
-   
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "product_brand: " . $row["product_brand"]. " product_name: " . $row["product_name"]. " " . $row["price"]. "<br>";
-  }
-}
-
-//SELECT SUM( price ) FROM shoppingcart WHERE customer_id = $customerid;
-
-//DELETE FROM `shoppingcart` WHERE customer_id = $customerid;
-
-$dbcnx->close();
 ?>
- <table border = "1">
+ <table border = "1" style = 'margin-left: auto; margin-right: auto;'>
    <thead>
     <tr>
       <th>Brand</th>
       <th>Product Name</th>
       <th>Price</th>
      </tr>
-     </thead>
-     <tbody>
-     <?php
-        $totalsum = "SELECT SUM( price ) FROM shoppingcart WHERE customer_id = $customerid";
-        for ($i = 0; $i < count($result); $i++){
-          echo '<tr>';
-          echo '<td>'.$row['product_brand']."<br></td>";
-          echo '<td>' .$row["product_name"]. "<br></td>";
-          echo '<td>' . $row["price"]."<br></td>";
-          echo '</tr>';
-        }
-      ?>
-   </tbody>
-   <tfoot>
-     <tr>
-       <th align="right">Total:</th><br>
-       <th align="right">$<?php echo number_format($totalsum, 2);?></th>
-     </tr>
-   </tfoot>
- </table>
+<?php
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<tr>
+            <td> {$row["product_brand"]} </td>
+            <td> {$row["product_name"]} </td>
+            <td>$ {$row["price"]} </td>
+          </tr>";
+  }
+}
+
+$query5 = 'SELECT price FROM shoppingcart' . " WHERE customer_id = '$customerid'";
+$result = $dbcnx->query($query5);
+$totalsum = 0;
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    $totalsum += $row['price'];
+  }
+}
+$_SESSION['totalsum'] = $totalsum;
+
+echo "<tr>
+        <th align='right' colspan='2'>Total: </th>
+        <td align='right'>$ {$totalsum}</td>
+      </tr>
+      </table>";
+
+if (isset($_GET['click'])) {
+  $query6 = 'DELETE FROM shoppingcart' . " WHERE customer_id = '$customerid'";
+  $result = $dbcnx->query($query6);
+  unset($_GET['click']);
+  echo '<script> window.location.href = "shoppingcart.php" </script>';
+}
+
+echo "<button class='button'><a href='shoppingcart.php?click=true'>Clear Cart</a></button>
+    <button class='button'><a href='payment.php'>Proceed to Payment</a></button>";
+
+$dbcnx->close();
+?>
 
 <footer></footer>
 <script src="js/footer.js"></script>
